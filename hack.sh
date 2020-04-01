58b22308e0707694eb87997dec894a8ecc5c78e2
Hacker65313 <53178827+Hacker65313@users.noreply.github.com>
2020-04-01 17:41:04 +0700

    Add files via upload

---
+++ hack.sh
@@ -0,0 +1,314 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+clear
+echo
+echo $i"###            ###    ##############
+###            ###    ##############
+###    #####   ###    ###        ###
+###   #######  ###    ###        ###
+###  ###  #### ###    ##############
+#######    #######    ##############
+######      ######    ###        ###
+#####        #####    ###        ###"
+echo $bi"Masukan Nomer Korban : "
+	read -p"  ---->" name
+sleep 2
+echo $me"sedang mengecek..."
+sleep 1
+echo $me"sedang mengecek..."
+sleep 15
+   echo $ku"Login Berhasil :" $name
+sleep 3
+clear
+echo $i"###            ###    ##############
+###            ###    ##############
+###    #####   ###    ###        ###
+###   #######  ###    ###        ###
+###  ###  #### ###    ##############
+#######    #######    ##############
+######      ######    ###        ###
+#####        #####    ###        ###"
+echo
+echo $i"1.Lihat Chat Korban"
+sleep 1
+echo $cy"Pilih Nomer "
+read -p"  ---->" name
+
+if [ $name = 1 ]
+then
+clear
+sleep 2
+echo $me"Loading..."
+fi
+
+sleep 2
+echo $ku"Sedang Crack Chat...."
+echo
+sleep 1
+echo $me"   [ Agak Lama ]"
+sleep 1
+echo
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+sleep 1
+echo $ku"Tunggu!!"
+clear
+echo $i"###            ###    ##############
+###            ###    ##############
+###    #####   ###    ###        ###
+###   #######  ###    ###        ###
+###  ###  #### ###    ##############
+#######    #######    ##############
+######      ######    ###        ###
+#####        #####    ###        ###"
+echo
+echo $ku"Berikut Chat Korban....."
+sleep 5
+echo
+echo $me"   ----[ HOME ]----"
+echo $ku"1.Adis"
+echo $ku"2.Regar"
+echo $ku"3.Tata"
+echo $ku"4.Exit"
+echo $ku"5.Chat Lainya"
+sleep 2
+echo $bi"Pilih Nomer :"
+	read -p" ---->" name
+
+if [ $name = 1 ]
+then
+clear
+sleep 1
+echo $i"###            ###    ##############
+###            ###    ##############
+###    #####   ###    ###        ###
+###   #######  ###    ###        ###
+###  ###  #### ###    ##############
+#######    #######    ##############
+######      ######    ###        ###
+#####        #####    ###        ###"
+sleep 2
+echo
+echo $bi"  Adis: p"
+echo $bi"  Aku : apa?"
+echo $bi"  Adis: boleh minta tolong?"
+echo $bi"  Adis: itu kalo lu mau"
+echo $bi"  Aku : minta tolong apa dis?"
+echo $bi"  Adis: PM in wa gue dong hhhha...."
+echo $bi"  Aku : yaelah pm,kaga ah ntar pada minta males gue?"
+echo $bi"  Adis: sekali aja dah,pliss..."
+echo $bi"  Aku : kaga ah males gua"
+echo $bi"  Adis: aelahh"
+echo $bi"  Adis: yaudah lah"
+sleep 2
+echo
+echo $pur"  ++++ TERIMAKASIH TELAH MENGGUNAKAN JASA SAYA ++++"
+sleep 2
+echo $pur"               [ jasa_hack_medsos ]"
+sleep 2
+echo $pur"              Jangan Lupa Order Lagi"
+sleep 2
+echo $pur"              25Semoga Membantu Ya kak"
+fi
+
+if [ $name = 2 ]
+then
+sleep 1
+echo $i"###            ###    ##############
+###            ###    ##############
+###    #####   ###    ###        ###
+###   #######  ###    ###        ###
+###  ###  #### ###    ##############
+#######    #######    ##############
+######      ######    ###        ###
+#####        #####    ###        ###"
+sleep 2
+echo
+echo $bi"  Aku   : gar ikut gue yuk"
+echo $bi"  Aku   : gue tlaktir"
+echo $bi"  Regar : kemana?"
+echo $bi"  Aku   : udah ikut aja dah luu"
+echo $bi"  Aku   : ntar gue jemput lu atau elu ke rumah gue?"
+echo $bi"  Regar : ok dah,lu aja kesini bengsin gue nipis"
+echo $bi"  Aku   : ok,ntar habis magrib ke situ gar"
+echo $bi"  Regar : siap"
+sleep 2
+echo
+echo $pur"  ++++ TERIMAKASIH TELAH MENGGUNAKAN JASA SAYA ++++"
+sleep 2
+echo $pur"               [ jasa_hack_medsos ]"
+sleep 2
+echo $pur"              Jangan Lupa Order Lagi"
+sleep 2
+echo $pur"              25Semoga Membantu Ya kak"
+fi
+
+if [ $name = 3 ]
+then
+sleep 1
+echo $i"###            ###    ##############
+###            ###    ##############
+###    #####   ###    ###        ###
+###   #######  ###    ###        ###
+###  ###  #### ###    ##############
+#######    #######    ##############
+######      ######    ###        ###
+#####        #####    ###        ###"
+sleep 2
+echo
+echo $bi"  Tata: oi"
+echo $bi"  Aku : apaan ta"
+echo $bi"  Tata: btw lagi apa luu"
+echo $bi"  Aku : lagi pingsan"
+echo $bi"  Tata: ditanya beneran gitu"
+echo $bi"  Aku : ya mainan hp lah,dah tau on wa gini"
+echo $bi"  Tata: yaelah gitu aja ngegas luu"
+echo $bi"  Aku : ngegas dikit lahh kaga papa"
+echo $bi"  Tata: kaga boleh lah"
+echo $bi"  Aku : iya dah"
+echo $bi"  Tata: lu kaga tidur,dah malam nihh"
+echo $bi"  Aku : kaga nagntuk gue,baru jam segini juga"
+echo $bi"  Tata: ini dah malam njir"
+echo $bi"  Aku : buat gue belum"
+echo $bi"  Tata: eh iya lu kan kelelwar"
+echo $bi"  Aku : Ngledek lu?"
+echo $bi"  Tata: Canda njir hhhe...."
+echo $bi"  Aku : hmmm"
+echo $bi"  Aku : ya udahlah gue mau makan dulu"
+echo $bi"  Tata: yaudah sono"
+sleep 2
+echo
+echo $pur"  ++++ TERIMAKASIH TELAH MENGGUNAKAN JASA SAYA ++++"
+sleep 2
+echo $pur"               [ jasa_hack_medsos ]"
+sleep 2
+echo $pur"              Jangan Lupa Order Lagi"
+sleep 2
+echo $pur"              25Semoga Membantu Ya kak"
+fi
+
+if [ $name = 4 ]
+then
+sleep 1
+echo $i"###            ###    ##############
+###            ###    ##############
+###    #####   ###    ###        ###
+###   #######  ###    ###        ###
+###  ###  #### ###    ##############
+#######    #######    ##############
+######      ######    ###        ###
+#####        #####    ###        ###"
+sleep 2
+echo
+echo $pur"  ++++ TERIMAKASIH TELAH MENGGUNAKAN JASA SAYA ++++"
+sleep 2
+echo $pur"               [ jasa_hack_medsos ]"
+sleep 2
+echo $pur"              Jangan Lupa Order Lagi"
+sleep 2
+echo $pur"              25Semoga Membantu Ya kak"
+fi
+
+if [ $name = 5 ]
+then
+sleep 1
+echo $i"###            ###    ##############
+###            ###    ##############
+###    #####   ###    ###        ###
+###   #######  ###    ###        ###
+###  ###  #### ###    ##############
+#######    #######    ##############
+######      ######    ###        ###
+#####        #####    ###        ###"
+sleep 2
+echo
+echo $me"       [ CATATAN ]"
+echo
+echo $me"Nomer 5 bug belum tersedia ya"
+echo $me"  insyaallah next pembuatan"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
